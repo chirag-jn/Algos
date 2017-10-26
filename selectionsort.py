bdb2b5f713bb0838587c0adec68fa790fbd86462
Chirag <chiragjn120@gmail.com>
2017-10-26 12:27:42 +0530

    Update selection sort, earlier wrong algorithm

--- selectionsort.py
+++ selectionsort.py
@@ -5,10 +5,13 @@
 arr=list(map(int,input().split()))
 total=len(arr)
 for i in range(total):
-	minindex=arr.index(min(arr[i:]))
+	least=i
+	for k in range(i+1,total):
+		if(arr[k]<arr[least]):
+			least=k
 	temp=arr[i]
-	arr[i]=arr[minindex]
-	arr[minindex]=temp
+	arr[i]=arr[least]
+	arr[least]=temp
 print(arr)
 
 # stable sort algo, inplace sorting algo
