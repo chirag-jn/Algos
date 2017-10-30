718f3073eafbe9ae9940bd16bdcaf479c13966c9
Chirag <chiragjn120@gmail.com>
2017-10-30 10:12:15 +0530

    Merge sort of sorted lists

---
+++ mergesortofsortedlists.py
@@ -0,0 +1,19 @@
+arr1=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+total1=len(arr1)
+total2=len(arr2)
+i=0
+j=0
+arr3=[]
+while (i<total1) and (j<total2):
+	if(arr1[i]<=arr2[j]):
+		arr3.append(arr1[i])
+		i+=1
+	elif(arr1[i]>arr2[j]):
+		arr3.append(arr2[j])
+		j+=1
+if(i==total1):
+	arr3=arr3+arr2[j:]
+if(j==total2):
+	arr3=arr3+arr1[i:]
+print(arr3)
