3614cae28d2616fa7b6d87ed988fba10ec59578f
Chirag <chiragjn120@gmail.com>
2017-11-01 13:19:19 +0530

    Final Merge Sort

--- mergesort.py
+++ mergesort.py
@@ -1,39 +1,33 @@
-# precondition, the two lists which are inputs are already sorted
+def merge(arr1,arr2):
+	total1=len(arr1)
+	total2=len(arr2)
+	i=0
+	j=0
+	arr3=[]
+	while (i<total1) and (j<total2):
+		if(arr1[i]<=arr2[j]):
+			arr3.append(arr1[i])
+			i+=1
+		elif(arr1[i]>arr2[j]):
+			arr3.append(arr2[j])
+			j+=1
+	if(i==total1):
+		arr3=arr3+arr2[j:]
+	if(j==total2):
+		arr3=arr3+arr1[i:]
+	return arr3
 
-def mergeSort(arr):
-	if len(arr)>1:
-		mid = len(arr)//2
-		lefthalf = arr[:mid]
-		righthalf = arr[mid:]
-		mergeSort(lefthalf)
-		mergeSort(righthalf)
-		i=0
-		j=0
-		k=0
-		while i < len(lefthalf) and j < len(righthalf):
-			if lefthalf[i] < righthalf[j]:
-				arr[k]=lefthalf[i]
-				i=i+1
-			else:
-				arr[k]=righthalf[j]
-				j=j+1
-			k=k+1
-		while i < len(lefthalf):
-			arr[k]=lefthalf[i]
-			i=i+1
-			k=k+1
-		while j < len(righthalf):
-			arr[k]=righthalf[j]
-			j=j+1
-			k=k+1
+def mergesort(A):
+	if(len(A)==1):
+		return A
+	# L: first half
+	# R: second half
+	L=A[0:len(A)//2]
+	R=A[len(A)//2:]
+	l=mergesort(L)
+	r=mergesort(R)
+	a=merge(l,r)
+	return a
 
 arr=list(map(int,input().split()))
-mergeSort(arr)
-print(arr)
-
-# its a stable sort, not an inplace sorting algo
-# in inplace sorting, extra space is constant
-# here extra space is total1+total2
-# so, extra space depends on problem size
-
-# merge sort can also work as a helper function, by dividing the lists into two both of which are sorted
+print(mergesort(arr))
