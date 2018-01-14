d72b488accab404f690be91d41d373c9d6028a48
Chirag Jain <jainchg@gmail.com>
2018-01-14 21:59:01 +0530

    Recursive Binary Search

--- recursive_binarysearch.py
+++ recursive_binarysearch.py
@@ -1,14 +1,14 @@
 def binarysearch(arr,x,l,r):
-	l=0
-	r=len(arr)-1
-	while(l<=r):
-		mid=(l+r)//2
-		if(arr[mid]==x):
-			return mid
-		elif(arr[mid]>x):
-			r=mid-1
-		else:
-			l=mid+1
-	return -1
+	mid=(l+r)//2
+	if(r<l):
+		return -1
+	if(arr[mid]==x):
+		return mid
+	elif(arr[mid]>x):
+		return binarysearch(arr,x,l,mid-1)
+	elif(arr[mid]<x):
+		return binarysearch(arr,x,mid+1,r)
 
-print(binarysearch(list(map(int,input().split())),int(input('Number to search: '))))
+arr = list(map(int,input("Enter Array: ").split()))
+elem = int(input("Enter Number to search: ")) 
+print(binarysearch(arr,elem,0,len(arr)-1))
